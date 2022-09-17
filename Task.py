357dcd30ee98ed6946fc981d5871d607ccbdcffe
Erkin000 <valiyaroverkin@mail.com>
2022-09-17 13:23:22 +0300

    project

---
+++ Task.py
@@ -0,0 +1,12 @@
+#Exercise 1
+# import os
+# # os.mkdir('Exam')
+# # os.mkdir('c://users/eldar/deskop/Exam')
+# #Exercise 2
+# os.rmdir('Exam')
+
+#Exercise 3
+import os
+# f = open('task_1.txt.txt', 'w')
+# f.write('Hello \nWorld')
+os.rename('task','renamefile.txt.txt')
